2ed833e7d95c7a87364a35af5b84928a73b708fa
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-01 22:10:28 -0400

    some more interactivity

--- carmen.py
+++ carmen.py
@@ -9,19 +9,23 @@ cities = {
         "3": "Chimney Rock"
         }
 
-choice = raw_input('Choose a number from 1 to 4: ')
+username = raw_input('What is your name? ')
+
+choice = raw_input('Carmen Sandiego has gone rogue! To find her lair, choose a number from 1 to 4: ')
+
+carmenis = "Carmen Sandiego is in "
 
 def choosepath(path):
     path = choice
     if path == '1':
-        return cities['0']
+        return carmenis + cities['0']
     if path == '2':
-        return cities['1']
+        return carmenis + cities['1']
     if path == '3':
-        return cities['2']
+        return carmenis + cities['2']
     if path == '4':
-        return cities['3']
+        return carmenis + cities['3']
     else:
-        return "hey, you did not follow the rules"
+        return "hey, you did not follow the rules, " + username
 
 print choosepath(choice)
