e0785e877fb374a91dee3dac6bea54f86946fbc1
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-01 22:16:28 -0400

    now actually a good skeleton for the guessing game

--- carmen.py
+++ carmen.py
@@ -9,23 +9,31 @@ cities = {
         "3": "Chimney Rock"
         }
 
+currentcity = "Independence"
+
 username = raw_input('What is your name? ')
+
+print "You are in " + currentcity
 
 choice = raw_input('Carmen Sandiego has gone rogue! To find her lair, choose a number from 1 to 4: ')
 
-carmenis = "Carmen Sandiego is in "
+carmenis = "You follow Carmen to "
 
 def choosepath(path):
     path = choice
     if path == '1':
-        return carmenis + cities['0']
+        currentcity = cities['0']
+        return carmenis + currentcity
     if path == '2':
-        return carmenis + cities['1']
+        currentcity = cities['1']
+        return carmenis + currentcity
     if path == '3':
-        return carmenis + cities['2']
+        currentcity = cities['2']
+        return carmenis + currentcity
     if path == '4':
-        return carmenis + cities['3']
+        currentcity = cities['3']
+        return carmenis + currentcity
     else:
-        return "hey, you did not follow the rules, " + username
+        return "hey, you did not follow the rules, " + username + ", so you stay in " + currentcity
 
 print choosepath(choice)
