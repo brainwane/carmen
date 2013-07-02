13676a8babee3b749d092edfa65344c4ef662699
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-01 21:29:09 -0400

    no need to make it an int anyway; checking for wrong choices better this way

--- carmen.py
+++ carmen.py
@@ -2,17 +2,17 @@
 import math
 
 
-choice = int(raw_input('Choose a number from 1 to 4: '))
+choice = raw_input('Choose a number from 1 to 4: ')
 
 def choosepath(path):
     path = choice
-    if path == 1:
+    if path == '1':
         return "cool, 1 is great!"
-    if path == 2:
+    if path == '2':
         return "you chose 2"
-    if path == 3:
+    if path == '3':
         return "you chose 3"
-    if path == 4:
+    if path == '4':
         return "you chose 4, that is weird"
     else:
         return "hey, you did not follow the rules"
