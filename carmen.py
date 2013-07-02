76549d26f4b724cc5f1a02801f4d524cc0a7891b
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-01 22:01:14 -0400

    attempting to use JSON

--- carmen.py
+++ carmen.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 import math
+import json
+import sys
 
+f = open(r'cities.json')
+# print places.read()
+
+places = json.load(f)
 
 choice = raw_input('Choose a number from 1 to 4: ')
 
 def choosepath(path):
     path = choice
     if path == '1':
-        return "cool, 1 is great!"
+        return places[0]
     if path == '2':
-        return "you chose 2"
+        return places[1]
     if path == '3':
         return "you chose 3"
     if path == '4':
