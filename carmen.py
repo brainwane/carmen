d79ff8c0cb2a534bd4040e311a76851e858a32bf
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-04 11:10:29 -0400

    starting to figure out the branching

--- carmen.py
+++ carmen.py
@@ -3,13 +3,6 @@ import math
 import sys
 import random
 
-cities = {
-        "1": "Virginia City, Montana",
-        "2": "South Pass, Wyoming",
-#        "3": "Fort Hall, Idaho",
-#        "4": "Salt Lake City, Utah"
-        }
-
 # time to construct the "tree" of possibilities as a dictionary of lists
 
 trails = { 
@@ -23,7 +16,7 @@ trails = {
 
 currentcity = "Fort Laramie"
 
-# I'll be randomizing which city Carmen is in later.
+# I'll be randomizing which city Carmen is in later; the "cities" dict no longer exists.
 # carmencity = cities[str(random.randrange(1,len(cities)))]
 
 carmencity = "Portland"
@@ -38,21 +31,29 @@ choice = raw_input('Which way to go? South Pass (SP) or Virginia City (VC)? ')
 
 followher = "You follow Carmen to "
 
-def choosepath(path):
+def firstchoice(path):
     path = choice
     global currentcity
     if path == 'VC':
-        currentcity = cities['1']
+        currentcity = "Virginia City"
         return followher + currentcity
     if path == 'SP':
-        currentcity = cities['2']
+        currentcity = "South Pass"
         return followher + currentcity
     else:
         return "That doesn't make sense, " + username + ", so you stay in " + currentcity + "."
 
-print choosepath(choice)
+print firstchoice(choice)
 
 if currentcity == carmencity:
     print "you win!"
 
-print "OK, now you are in " + currentcity + "."
+if currentcity == trails["Bozeman trail"][1]:
+    print "You are on the Bozeman trail. Wrong turn! No sign of Carmen here; time to head back."
+#    currentcity = "Fort Laramie"
+#    secondchance = raw_input('Which way to go? South Pass (SP) or Virginia City (VC)? ')
+#    print firstchoice(secondchance)
+
+if currentcity == trails["Usual trail"][1]:
+    print "You are on the right track! From here, the Mormon trail or the Gentile trail?"
+
