c3071c50d0c74ca049df939a346cd227b31891b7
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-04 17:20:02 -0400

    ok, let's try a Person class

--- carmen.py
+++ carmen.py
@@ -9,6 +9,11 @@ class City(object):
         self.name = name
         self.clue = clue
 
+class Person(object):
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
 ftl, vc, sp, slc, fh, pdx = (City("Fort Laramie", "Matthew Shepard"),
                              City("Virginia City", "a city in Montana named after an eastern State"),
                              City("South Pass", "a town named South you have to go north to get to"),
@@ -22,41 +27,35 @@ sp.dests = (slc, fh)
 slc.dests = (fh,)
 fh.dests = (pdx,)
 
-currentcity = ftl
-# I'll be randomizing which city Carmen is in later; the "cities" dict no longer exists.
-# carmencity = cities[str(random.randrange(1,len(cities)))]
-
-carmencity = pdx
-
+player = Person("none", ftl)
+carmen = Person("Carmen Sandiego", pdx)
 followher = "You follow Carmen to "
 
-username = raw_input('What is your name? ')
+player.name = raw_input('What is your name? ')
 
-print "You are in " + currentcity.name + ", and your possible destinations are:"
-print [x.name for x in currentcity.dests]
+print [x.name for x in player.location.dests]
 
-print "Carmen Sandiego has stolen a wagon tongue and we must catch her!"
-for i,x in enumerate(currentcity.dests):
+print carmen.name " has stolen a wagon tongue and we must catch her! Your choices are:"
+for i,x in enumerate(player.location.dests):
     print x.name + " is choice #" + str(i+1)
 
 choice = raw_input('Which way to go? ')
 
 def firstchoice(path):
     path = int(path)
-    if path not in range(1, (len(currentcity.dests)+1)):
-        return "That doesn't make sense, " + username + ", so you stay in " + currentcity.name + "."
+    if path not in range(1, (len(player.location.dests)+1)):
+        return "That doesn't make sense, " + player.name + ", so you stay in " + player.location.name + "."
     else:
-        global currentcity
-        path = currentcity.dests[path]
-        currentcity = path
-        return followher + currentcity.name
+        path = player.location.dests[path]
+        player.location = path
+        return followher + player.location.name
 
 print firstchoice(choice)
 
-if currentcity == carmencity:
+if player.location == carmen.location:
     print "you win!"
 
-print "You are now in " + currentcity.name
+print "You are now in " + player.location.name
 
 # def chooseatrail(thecity, listoftrails):
 #    for item in listoftrails:
