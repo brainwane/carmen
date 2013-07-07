4c1e07be9145ad8c8c3fbea2f0141028b927e1b5
Sumana Harihareswara <sumanah@wikimedia.org>
2013-07-06 21:04:32 -0400

    carmen moves around; providing clues; refactored player turns

--- TODO.md
+++ TODO.md
@@ -3,5 +3,3 @@ TODO
 
 1. Actually create & traverse tree of trails properly
 2. If you're in the wrong city, ask for the next guess
-3. Provide clues that help you go towards the correct city
-4. Carmen chooses, after each player turn, what city to go to next
--- carmen.py
+++ carmen.py
@@ -25,28 +25,18 @@ ftl, vc, sp, slc, fh, pdx = (City("Fort Laramie", "she had a lot of questions ab
 
 ftl.dests = (vc, sp)
 vc.dests = (ftl,)
-sp.dests = (slc, fh)
-slc.dests = (fh,)
-fh.dests = (pdx,)
+sp.dests = (ftl, slc, fh)
+slc.dests = (sp, fh)
+fh.dests = (sp, slc, pdx)
+pdx.dests = (fh,)
 
 player = Person("none", ftl)
 carmen = Person("Carmen Sandiego", random.choice((ftl, vc, sp, slc, fh, pdx)))
 followher = "You follow Carmen to "
 
-player.name = raw_input('What is your name? ')
-
-print carmen.name + " has stolen a wagon tongue and we must catch her!"
-print "You are in " + player.location.name + " and, after some asking around, you have learned that " + carmen.location.clue
-
-print "Your choices are:"
-
 def where2go():
     for i,x in enumerate(player.location.dests):
         print str(i+1) + ". "+ x.name
-
-where2go()
-
-choice = raw_input('Which way to go? ')
 
 def choose(path):
     # exception for if int(path) fails
@@ -57,20 +47,24 @@ def choose(path):
         player.location = player.location.dests[path-1]
         return followher + player.location.name
 
-print choose(choice)
-
 def wincondition():
     if player.location == carmen.location:
         print "You win!"
         sys.exit()
 
-def latterchoices():
+def playturn():
 	print player.name + ", you are now in " + player.location.name + " and you can head to:"
 	where2go()
 	print "You ask around about Carmen and learn that " + carmen.location.clue
-	choice = raw_input('OK, now which way? ')
+	choice = raw_input('OK, now which way will you go? ')
 	print choose(choice)
 
+player.name = raw_input('What is your name? ')
+print "Okay, " + player.name + ", welcome to " + player.location.name + "."
+print carmen.name + " has stolen a wagon tongue and we must catch her!"
+
 while player.location !=carmen.location:
-    latterchoices()
+    playturn()
+    wincondition()
+    carmen.location = random.choice(carmen.location.dests)
     wincondition()
